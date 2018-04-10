b89b6effdd12092cc9259b88a1ba4f39151877dc
Leonis Lamarck <lamarck.ti@gmail.com>
2018-04-10 08:14:08 -0300

    Mudanças complementares: adição de pontuação, número randômico, etc

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ATTEMPTS 3;
 
@@ -14,36 +16,51 @@ int main(){
 	printf("*** Bem vindo ao Jogo de Adivinhação! ***\n");
 	printf("*****************************************\n");
 
-	int secret_number = 42;
+	srand(time(0));
+	int secret_number = rand() % 100;
 	int guess;
 	int hit;
 	int i; //bug do Eclipse; lembrar de mudar a especificação para C99 ao invés de C89
+	int bigger;
+	//int win = 0;
+	int attempts = 1;
+	double score = 1000;
+	double damage;
 
 	//for (int i = 1; i <= 3; i++){
-	  for (i = 1; i <= /*ATTEMPTS*/ 3; i++){ //não usar constantes por enquanto, bug
-		printf("Qual o seu %dº chute?", i, "\n");
+	  //for (i = 1; i <= /*ATTEMPTS*/ 3; i++){ //não usar constantes por enquanto, bug
+	while(/*!win*/ 1){
+		printf("Qual o seu %dº chute?", attempts, "\n");
 		fflush(stdout); //forma de liberar o buffer do Eclipse
 		scanf("%d", &guess);
 		fflush(stdout);
 		printf("Você chutou o número %d!\n", guess);
 		hit = guess == secret_number;
+		bigger = guess > secret_number;
+		if(guess < 0){
+			printf("Você não pode chutar números negativos!");
+			//i--;
+			continue;
+		}
 
 		//if (secret_number == guess){
 		if (hit){
 			printf("Parabéns, você acertou!\n");
 			printf("Jogue de novo, você é um bom jogador!\n");
+			printf("Você fez %.2f pontos em %d tentativas! \n", score, attempts);
 			break;
+			//win = 1;
+		}else if (bigger){
+			printf("Que pena, você errou!\n");
+			printf("Dica: seu número foi maior que o número secreto!\n");
 		}else{
-			int bigger = guess > secret_number;
-			if (bigger){
-				printf("Que pena, você errou!\n");
-				printf("Dica: seu número foi maior que o número secreto!\n");
-			}else{
-				printf("Que pena, você errou!\n");
-				printf("Dica: seu número foi menor que o número secreto!\n");
-			}
+			printf("Que pena, você errou!\n");
+			printf("Dica: seu número foi menor que o número secreto!\n");
 		}
-	}
 
+		damage = abs(guess - secret_number)/2.0;
+		score = score - damage;
+		attempts++;
+	  }
 	return 0;
-}
+	}
